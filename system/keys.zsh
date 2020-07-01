6275c2145aef1fb23e32c3a742901da2669a9a05
noahbjohnson <noah@noahbjohnson.net>
2020-07-01 16:52:11 -0500

    added gpg ssh support

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,2 +1,4 @@
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
