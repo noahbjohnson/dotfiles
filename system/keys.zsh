2e7617ff45643aa6442adcd9acfc24b7157d8808
noahbjohnson <noah@noahbjohnson.net>
2020-07-01 16:54:55 -0500

    comment

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,4 +1,6 @@
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+
+# Use SSH gpg subkeys
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
