fc13ea26fbded25018571741a01c05e287d94539
Noah B Johnson <noah@noahbjohnson.net>
2020-09-01 23:21:29 -0500

    disabled zsh autocorrect

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,6 +3,3 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
-
-# Disable autocorrect
-DISABLE_CORRECTION="true"
